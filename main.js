9f669fe157f97e3f2ab679698c0ad6372250dfa0
Sara-Barbi <barbisara02@gmail.com>
2022-01-17 15:13:10 +0100

    array di oggetti

---
+++ main.js
@@ -0,0 +1,34 @@
+let app = new Vue ({
+ 
+    el: "#root",
+    data: {
+        arrayToDo :[
+            primoTo = {
+                text :" Quarterly newsletter",
+                todo : false ,
+            },
+            secondoTo = {
+                text :" Recluiting blog post",
+                todo : true ,
+            },            
+            terzoTo = {
+                text :" Mobile app Luaunch",
+                todo : true ,
+            },            
+            quartoTo = {
+                text :" Interview John H.",
+                todo : false ,
+            },            
+            quintoTo = {
+                text :" Summit updates to mobile storefronts ",
+                todo : true ,
+            },            
+            sestoTo = {
+                text :"Schedule meeting with Alex ",
+                todo : false ,
+            },
+        
+        ]
+    }
+    
+});
